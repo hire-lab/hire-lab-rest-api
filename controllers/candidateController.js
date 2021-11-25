efc85a6dfd753461c60f4a90d9883a00b9726ef6
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-11-25 12:36:41 +0200

    Update candidateController.js

--- controllers/candidateController.js
+++ controllers/candidateController.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
+const preload = require('../middlewares/preload')
 
 router.get('/', async (req, res) => {
     const candidates = await req.candidateStorage.getAll();
     res.json(candidates)
+})
+
+router.get('/:id', preload(), async (req, res) => {
+    const candidate = await req.candidateStorage.getOne()
+    res.json(candidate)
 })
 
 module.exports = router;
