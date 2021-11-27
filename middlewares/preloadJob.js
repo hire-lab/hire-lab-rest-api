8129fc0575935e1ebadce1afaac3b939afc3585b
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-11-27 11:26:57 +0200

    Update preloadJob.js

--- middlewares/preloadJob.js
+++ middlewares/preloadJob.js
@@ -2,7 +2,7 @@ module.exports = (paramName = 'id') => async (req, res, next) => {
     const id = req.params[paramName];
 
     try {
-        const data =  await req.storage.getOne(id);
+        const data =  await req.storage.getById(id);
         if (!data) {
             throw new Error('Not found')
         }
