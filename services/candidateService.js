23930069fe466abedfbf13399e30291116b4764a
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-11-28 07:31:25 +0200

    Update candidateService.js

--- services/candidateService.js
+++ services/candidateService.js
@@ -8,7 +8,14 @@ async function getOne(id){
     return await Candidate.findById(id).lean()
 }
 
+async function create(data) {
+    const result = new Candidate(data)
+    await result.save()
+    return result;
+}
+
 module.exports = {
     getAll,
-    getOne
+    getOne,
+    create
 }
