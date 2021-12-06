f7685be2599a3dc00ac8aef380dd29878fadd0fb
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-12-06 11:13:54 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const {Schema, model} = require('mongoose')
 
 const schema = new Schema({
-    email: {type: String, required: true},
+    email: {type: String, required: true, match: [/(?:\@company\.com)/, 'Please provide valid company\'s email address']},
     name: {type: String, required: true},
     hashedPassword:  {type: String}
 })
