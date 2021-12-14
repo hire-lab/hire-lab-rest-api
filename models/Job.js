08c77f63d272bb303d46bf21b8cd493044536483
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-12-14 22:46:35 +0200

    Update Job.js

--- models/Job.js
+++ models/Job.js
@@ -4,8 +4,7 @@ const {Schema, model} = require('mongoose')
 const schema = new Schema({
     title: {type: String, required: true},
     description: {type: String, required: true},
-    potenitalCandidates: [{type: Schema.Types.ObjectId, ref: 'User'}],
-    owner: {type: Schema.Types.ObjectId, ref: 'User'}
+    potenitalCandidates: [{type: Schema.Types.ObjectId, ref: 'User'}]
 })
 
 module.exports = model('Job', schema)
