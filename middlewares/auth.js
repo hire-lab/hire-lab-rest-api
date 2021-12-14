71e1042ffa125c797fc6b61f1ac7ccd2770d0db8
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-12-14 16:38:24 +0200

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 const { SECRET } = require('../config/')
 
 module.exports = () => (req, res, next) => {
-    const token = req.headers['x-authorization'];
+    const token = req.headers['X-Authorization'];
 
     try {
         if (token) {
