9959b95a50b82637f0f1b1be40dd4a80a5645bb0
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-12-17 21:06:42 +0200

    Update jobService.js

--- services/jobService.js
+++ services/jobService.js
@@ -14,10 +14,13 @@ async function create(data) {
     return result;
 }
 
-async function update(original, updated) {
-    Object.assign(original, updated)
-    await original.save()
-    return original;
+async function update(id, updated) {
+    const job = await Job.findById(id)
+
+    job.title = updated.title;
+    job.description = updated.description;
+
+    return job.save()
 }
 
 async function remove(id){
