c039d933be94cb5306b7f3fcd95d5f2651f2125a
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2022-01-07 09:58:49 +0200

    Delete Interview.js

--- models/Interview.js
+++
@@ -1,9 +0,0 @@
-const {Schema, model} = require('mongoose')
-
-
-const schema = new Schema({
-    title: {type: String, required: true},
-    interviewingCandidates: [{type: Schema.Types.ObjectId, ref: 'Candidate'}]
-})
-
-module.exports = model('Interview', schema)
