59b7228ed1ab7c81fcfef82dca85b11d50580f1d
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2022-01-10 08:36:53 +0200

    Update candidateService.js

--- services/candidateService.js
+++ services/candidateService.js
@@ -9,6 +9,14 @@ async function getOne(id){
 }
 
 async function create(data) {
+    const email = data.email;
+    const existing = await Candidate.findOne({email}).lean();
+    if (existing){
+        const err = new Error('Email is already in use');
+        //err.status(409);
+        throw err;
+    }
+
     const result = new Candidate(data)
     await result.save()
     return result;
