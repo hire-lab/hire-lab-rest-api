f55f3cb8179f43a523e31e6194852f9c4621da54
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2022-02-02 16:53:28 +0200

    Update interviewService.js

--- services/interviewService.js
+++ services/interviewService.js
@@ -1,4 +1,5 @@
 const Interview = require('../models/Intervew');
+const Job = require('../models/Job');
 
 async function getAll(filter) {
     return await Interview.find(filter).lean();
@@ -12,15 +13,23 @@ async function getCandidateInterviews(candidateId) {
     return await Interview.find({ candidateId }).lean();
 }
 
-/*async function bookInterview(jobId){
-    const result = new Interview(jobId);
-    await result.save()
+async function bookInterview(interviewData){
+    const jobId = interviewData.job;
+    const candidateId = interviewData.potenitalCandidates;
+
+    const job = await Job.findById(jobId).lean();
+    job.potenitalCandidates.push(candidateId);
+    await job.save()
+
+    const result = new Interview(interviewData);    
+    await result.save();
     return result;
-}*/
+}
 
 
 module.exports = {
     getAll,
     getInterviewsByJobId,
-    getCandidateInterviews
+    getCandidateInterviews,
+    bookInterview
 }
