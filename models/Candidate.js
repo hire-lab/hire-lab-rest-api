32615a1e8047e83e58009dcb601aacaa1a8d9538
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2022-02-02 16:52:35 +0200

    Update Candidate.js

--- models/Candidate.js
+++ models/Candidate.js
@@ -3,7 +3,7 @@ const {Schema, model} = require('mongoose')
 const schema = new Schema ({
     name: {type: String, required: true},
     email: {type: String, required: true},
-    interview: {type: Schema.Types.ObjectId, ref: 'Job'}
+    interview: {type: Schema.Types.ObjectId, ref: 'Interview'}
 })
 
 module.exports = model('Candidate', schema)
