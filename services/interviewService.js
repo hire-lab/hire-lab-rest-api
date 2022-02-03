3ab3a51f294d7407f1400e60cb5721e8f67844f3
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2022-02-03 10:15:40 +0200

    Add files via upload

--- services/interviewService.js
+++ services/interviewService.js
@@ -2,7 +2,7 @@ const Interview = require('../models/Intervew');
 const Job = require('../models/Job');
 
 async function getAll(filter) {
-    return await Interview.find(filter).lean();
+    return await Interview.find({filter}).lean();
 }
 
 async function getInterviewsByJobId(jobId) {
@@ -14,13 +14,6 @@ async function getCandidateInterviews(candidateId) {
 }
 
 async function bookInterview(interviewData){
-    const jobId = interviewData.job;
-    const candidateId = interviewData.potenitalCandidates;
-
-    const job = await Job.findById(jobId).lean();
-    job.potenitalCandidates.push(candidateId);
-    await job.save()
-
     const result = new Interview(interviewData);    
     await result.save();
     return result;
