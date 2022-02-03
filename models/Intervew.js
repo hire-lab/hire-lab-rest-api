82f09432324febff90d68ff5f61610a36df3c4d1
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2022-02-03 14:35:49 +0200

    Update Intervew.js

--- models/Intervew.js
+++ models/Intervew.js
@@ -2,7 +2,9 @@ const {Schema, model} = require('mongoose');
 
 const schema = new Schema({
     job: {type: Schema.Types.ObjectId, ref: 'Job'},
-    potentialCandidates: [{type: Schema.Types.ObjectId, ref: 'Candidate'}]
+    potentialCandidates: [{type: Schema.Types.ObjectId, ref: 'Candidate'}],
+    jobTitle: {type: String},
+    candidateName: {type: String}
 })
 
 module.exports = model('Interview', schema)
