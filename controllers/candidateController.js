c57b3571ae69446ff7f2a7140044b3c19af3bdf9
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2022-02-07 17:32:55 +0200

    Update candidateController.js

--- controllers/candidateController.js
+++ controllers/candidateController.js
@@ -11,15 +11,6 @@ router.get('/', async (req, res) => {
 
 })
 
-router.get('/:id', preloadCandidate(), (req, res) => {
-    try {
-        const candidate = req.data;
-        res.json(candidate)
-    } catch (err) {
-        res.status(err.status || 400).json({message: err.message})
-    }
-})
-
 router.post('/', async(req, res) => {
     const candidateData = {
         name: req.body.name,
@@ -34,5 +25,27 @@ router.post('/', async(req, res) => {
     }
 })
 
+router.get('/:id', preloadCandidate(), (req, res) => {
+    try {
+        const candidate = req.data;
+        res.json(candidate)
+    } catch (err) {
+        res.status(err.status || 400).json({message: err.message})
+    }
+})
+
+router.put('/:id', preloadCandidate(), async (req, res) => {
+    const updated = {
+        name: req.body.name,
+        email: req.body.email
+    }
+
+    try {
+        const result = await req.candidateStorage.update(req.params.id, updated);
+        res.json(result)
+    } catch (err) {
+        res.status(err.status || 400).json({message: err.message})
+    }
+})
 
 module.exports = router;
