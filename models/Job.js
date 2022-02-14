1e2438187fdafd9113db920adb4ea38e5aa8f5fa
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2022-02-14 16:17:37 +0200

    Add files via upload

--- models/Job.js
+++ models/Job.js
@@ -2,7 +2,10 @@ const {Schema, model} = require('mongoose')
 
 const schema = new Schema({
     title: {type: String, required: true},
-    description: {type: String, required: true}
+    companyName: {type: String, required: true},
+    description: {type: String, required: true},
+    location: {type: String, required: true},
+    companyId: {type: Schema.Types.ObjectId, ref: 'Company'}
 })
 
 module.exports = model('Job', schema)
