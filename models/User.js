5546217889761823b044a0b657c569f90d88e1e8
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2022-02-16 15:56:01 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const {Schema, model} = require('mongoose')
 const schema = new Schema({
     email: {type: String, required: true},
     name: {type: String, required: true},
+    cv: {type: String, required: true},
     hashedPassword:  {type: String}
 })
 
