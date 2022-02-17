7a0482f27058643dd30342ea6ea08a0d49ce0ec6
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2022-02-17 08:42:11 +0200

    Add files via upload

--- models/Candidate.js
+++ models/Candidate.js
@@ -3,6 +3,7 @@ const {Schema, model} = require('mongoose')
 const schema = new Schema ({
     name: {type: String, required: true},
     email: {type: String, required: true},
+    cv: {type: String, required: true},
     jobId: [{type: Schema.Types.ObjectId, ref: 'Job'}],
     companyId: [{type: Schema.Types.ObjectId, ref: 'Company'}]
 })
