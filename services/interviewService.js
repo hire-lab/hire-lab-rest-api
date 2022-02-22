5438588eeb1503d6cc3a862cd074550aab49c602
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2022-02-22 12:21:17 +0200

    updated Interview model

--- services/interviewService.js
+++ services/interviewService.js
@@ -1,12 +1,11 @@
 const Interview = require('../models/Intervew');
-const Candidate = require('../models/Candidate');
 
-async function getAll() {
-    return await Interview.find({}).lean();
+async function getAll(companyId) {
+    return await Interview.find({companyId: companyId}).populate('candidateId').populate('jobId').lean();
 }
 
 async function getInterviewsByJobId(jobId) {
-    return await Interview.find({job: jobId}).lean();
+    return await Interview.find({jobId: jobId}).populate('candidateId').populate('jobId').lean()
 }
 
 /*async function getCandidateInterviews(candidateId) {
@@ -14,10 +13,20 @@ async function getInterviewsByJobId(jobId) {
 }*/
 
 async function bookInterview(interviewData){ 
-    const candidateId = interviewData.potentialCandidates;
+    /*const existingCandidate = await Interview.find({candidateId: interviewData.candidateId})
+    const existingJob = await Interview.find({jobId: interviewData.jobId})
+
+    console.log(existingCandidate)
+    if (existingCandidate){
+        if (existingJob){
+            const err = new Error('Candidate has an interview already');
+            throw err;
+        }
+    }*/
+
     const result = new Interview(interviewData);    
     await result.save();
-    await Candidate.findByIdAndDelete(candidateId)
+    //await Candidate.findByIdAndDelete(candidateId)
     return result;
 }
 
