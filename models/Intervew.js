920964811d8fa1da150ccceb7c897450c1a3813c
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2022-02-22 17:03:32 +0200

    Add files via upload

--- models/Intervew.js
+++ models/Intervew.js
@@ -3,7 +3,9 @@ const {Schema, model} = require('mongoose');
 const schema = new Schema({
     jobId: {type: Schema.Types.ObjectId, ref: 'Job'},
     candidateId: {type: Schema.Types.ObjectId, ref: 'Candidate'},
-    companyId: {type: Schema.Types.ObjectId, ref: 'Company'}
+    companyId: {type: Schema.Types.ObjectId, ref: 'Company'},
+    date: {type: Date},
+    time: {type: String}
 })
 
 module.exports = model('Interview', schema)
