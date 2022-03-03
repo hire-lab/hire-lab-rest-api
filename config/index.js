970a763c0b24ee87d9574e0bfb2952f746abd321
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2022-03-03 12:48:56 +0200

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+
 module.exports = {
-    PORT: process.env.PORT || 5500,
-    DB_CONNECTION_STRING: 'mongodb+srv://Zlatina123:Lorian696969@cluster0.ck6ia.mongodb.net/hire-lab?retryWrites=true&w=majority',
-    SECRET: 'Super Safe 99'
+    PORT: process.env.PORT || 5555,
+    DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
+    SECRET: process.env.SECRET
 }
